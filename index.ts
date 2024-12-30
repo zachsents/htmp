bb4effb5d8e42ef136ec8623af72ecb85a2fbdd5
Zach Sents <zachsents@gmail.com>
2024-12-30 09:26:31 -0500

    clutter fix

--- index.ts
+++ index.ts
@@ -4,77 +4,7 @@ import type { Attributes, Content, Node, NodeTag } from "posthtml-parser"
 import { render } from "posthtml-render"
 import * as prettier from "prettier"
 import { parseHtml } from "./lib/parser"
-
-export interface HTempCompileOptions {
-    /**
-     * Optional object containing components. These will take precedence over
-     * files in the componentsRoot.
-     */
-    components?: Record<string, string>
-    /**
-     * Where to look for components, which are just .html files.
-     * Defaults to "./components"
-     */
-    componentsRoot?: string
-    /**
-     * The prefix to use for component tags.
-     * Defaults to "x-"
-     */
-    componentTagPrefix?: string
-    /**
-     * The tag to use for yields.
-     * Defaults to "yield"
-     */
-    yieldTag?: string
-    /**
-     * Whether to pretty-print the output. Uses prettier.
-     * Defaults to true
-     */
-    pretty?: boolean
-    /**
-     * The attribute to use for passing attributes to components.
-     * Defaults to "attr"
-     */
-    attrAttribute?: string
-    /**
-     * The tag to use for defining slots.
-     * Defaults to "slot"
-     */
-    defineSlotTag?: string
-    /**
-     * The tag to use for filling slots.
-     * Defaults to "fill"
-     */
-    fillSlotTag?: string
-    /**
-     * The tag to use for defining stacks.
-     * Defaults to "stack"
-     */
-    stackTag?: string
-    /**
-     * The tag to use for pushing to stacks.
-     * Defaults to "push"
-     */
-    pushTag?: string
-    /**
-     * The prefix for attributes that will be evaluated as
-     * JavaScript.
-     * Defaults to "eval"
-     */
-    evaluateAttributePrefix?: string
-    /**
-     * The pattern used to select content for evaluation.
-     * The JS code should be contained in the first capture
-     * group.
-     * Defaults to `/%%(.+?)%%/g`
-     */
-    evaluateContentPattern?: RegExp
-    /**
-     * The tag used for dynamic content.
-     * Defaults to "dynamic"
-     */
-    dynamicTag?: string
-}
+import type { HTempCompileOptions } from "./lib/options"
 
 export async function compile(
     html: string,
@@ -109,7 +39,7 @@ export async function compile(
     )
 
     // Do initial evaluations
-    const evaluationOptions = {
+    const evaluationOptions: Parameters<typeof expandEvaluations>[1] = {
         evaluateAttributePrefix,
         evaluateContentPattern,
         dynamicTag,
@@ -569,3 +499,5 @@ function indirectEval(str: string) {
 type WalkReturn = Node | Node[] | Promise<Node | Node[]>
 
 type NodeTagWithTag = Omit<NodeTag, "tag"> & { tag: string }
+
+export type { HTempCompileOptions }
---
+++ lib/options.ts
@@ -0,0 +1,70 @@
+export interface HTempCompileOptions {
+    /**
+     * Optional object containing components. These will take precedence over
+     * files in the componentsRoot.
+     */
+    components?: Record<string, string>
+    /**
+     * Where to look for components, which are just .html files.
+     * Defaults to "./components"
+     */
+    componentsRoot?: string
+    /**
+     * The prefix to use for component tags.
+     * Defaults to "x-"
+     */
+    componentTagPrefix?: string
+    /**
+     * The tag to use for yields.
+     * Defaults to "yield"
+     */
+    yieldTag?: string
+    /**
+     * Whether to pretty-print the output. Uses prettier.
+     * Defaults to true
+     */
+    pretty?: boolean
+    /**
+     * The attribute to use for passing attributes to components.
+     * Defaults to "attr"
+     */
+    attrAttribute?: string
+    /**
+     * The tag to use for defining slots.
+     * Defaults to "slot"
+     */
+    defineSlotTag?: string
+    /**
+     * The tag to use for filling slots.
+     * Defaults to "fill"
+     */
+    fillSlotTag?: string
+    /**
+     * The tag to use for defining stacks.
+     * Defaults to "stack"
+     */
+    stackTag?: string
+    /**
+     * The tag to use for pushing to stacks.
+     * Defaults to "push"
+     */
+    pushTag?: string
+    /**
+     * The prefix for attributes that will be evaluated as
+     * JavaScript.
+     * Defaults to "eval"
+     */
+    evaluateAttributePrefix?: string
+    /**
+     * The pattern used to select content for evaluation.
+     * The JS code should be contained in the first capture
+     * group.
+     * Defaults to `/%%(.+?)%%/g`
+     */
+    evaluateContentPattern?: RegExp
+    /**
+     * The tag used for dynamic content.
+     * Defaults to "dynamic"
+     */
+    dynamicTag?: string
+}
