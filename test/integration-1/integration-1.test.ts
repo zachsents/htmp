311c0cf01f1ea92ed99715bb4437ef8c8cc73361
Zach Sents <zachsents@gmail.com>
2024-12-31 22:17:10 -0500

    fixed integration test

--- test/integration-1/integration-1.test.ts
+++ test/integration-1/integration-1.test.ts
@@ -1,8 +1,32 @@
 import { HTmpCompiler } from "../.."
+import { expect, test } from "bun:test"
 
-const html = await new HTmpCompiler({
-    componentsRoot: "./test/integration-1/components",
-    pretty: true,
-}).compile("<x-main />")
+test("Integration test 1", async () => {
+    const html = await new HTmpCompiler({
+        componentsRoot: "./test/integration-1/components",
+        pretty: true,
+    }).compile("<x-main />")
 
-console.log(html)
+    const compareTo = `<header class="header main-header">
+    <h1>Hello!</h1>
+</header>
+<main>
+    <article class="max-w-sm mx-auto main-article">
+    <h2>Integration Test</h2>
+    <p>This demonstrates integration with components.</p>
+    </article>
+
+    <ul>
+    <li>Item 1</li>
+
+    <li>Item 2</li>
+
+    <li>Item 3</li>
+    </ul>
+</main>
+<footer class="footer">
+    <p>&amp;copy; 2025 Your App. All rights reserved.</p>
+</footer>`
+
+    expect(html.replaceAll(/\s+/g, "")).toBe(compareTo.replaceAll(/\s+/g, ""))
+})
